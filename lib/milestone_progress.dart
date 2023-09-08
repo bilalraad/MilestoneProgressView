6334c326222733341f0ad28a92d0b088b8831663
Bilal raad <bilal52040@gmail.com>
2023-09-09 01:11:26 +0300

    feat: :sparkles: added suppoty for titles and weight factory constructor

--- lib/milestone_progress.dart
+++ lib/milestone_progress.dart
@@ -17,32 +17,58 @@ class MilestoneProgress extends StatelessWidget {
   final IconData nonCompletedIconData;
   final Color completedIconColor;
   final Color incompleteIconColor;
+  final List<String>? titles;
 
   MilestoneProgress({
-    required this.width,
     this.maxIconSize = 24,
+    required this.width,
     required this.totalMilestones,
     required this.completedMilestone,
     this.completedIconData = Icons.check_circle,
     this.nonCompletedIconData = Icons.adjust,
     this.completedIconColor = Colors.green,
     this.incompleteIconColor = Colors.grey,
-  });
+    this.titles,
+  }) : assert(totalMilestones == (titles?.length ?? totalMilestones));
+
+  factory MilestoneProgress.weight({
+    required double width,
+    required List<int> milestones,
+    required int weight,
+    double maxIconSize = 24,
+    IconData completedIconData = Icons.check_circle,
+    IconData nonCompletedIconData = Icons.adjust,
+    Color completedIconColor = Colors.green,
+    Color incompleteIconColor = Colors.grey,
+    List<String>? titles,
+  }) {
+    return MilestoneProgress(
+      totalMilestones: milestones.length,
+      width: width,
+      completedMilestone: countCompletedMilestones(milestones, weight),
+      completedIconColor: completedIconColor,
+      completedIconData: completedIconData,
+      nonCompletedIconData: nonCompletedIconData,
+      incompleteIconColor: incompleteIconColor,
+      maxIconSize: maxIconSize,
+      titles: titles,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: getTitleWithMilestone(
-        width,
-        totalMilestones,
-        completedMilestone,
-        completedIconData,
-        nonCompletedIconData,
-        completedIconColor,
-        incompleteIconColor,
-        maxIconSize,
-      ),
+          width,
+          totalMilestones,
+          completedMilestone,
+          completedIconData,
+          nonCompletedIconData,
+          completedIconColor,
+          incompleteIconColor,
+          maxIconSize,
+          titles),
     );
   }
 }
@@ -56,6 +82,7 @@ List<Widget> getTitleWithMilestone(
   Color completedIconColor,
   Color incompleteIconColor,
   double mxIconSize,
+  List<String>? titles,
 ) {
   List<Widget> list = [];
   double iconSizeTemp = width / (1.5 * totalPoints);
@@ -68,31 +95,44 @@ List<Widget> getTitleWithMilestone(
   double lineSize = lineSizeTemp > maxLineSize ? maxLineSize : lineSizeTemp;
 
   for (int i = 0; i < (2 * totalPoints) - 1; i++) {
+    final index = (2 * checkedPoints) - 1;
     if (i % 2 == 0) {
       list.add(
-        Icon(
-          i <= (2 * checkedPoints) - 1
-              ? completedIconData
-              : nonCompletedIconData,
-          size: iconSize,
-          color: i <= (2 * checkedPoints) - 1
-              ? completedIconColor
-              : incompleteIconColor,
+        Column(
+          children: [
+            Icon(
+              i <= index ? completedIconData : nonCompletedIconData,
+              size: iconSize,
+              color: i <= index ? completedIconColor : incompleteIconColor,
+            ),
+            if (titles != null)
+              SizedBox(
+                height: 20,
+                child: Text(titles[i ~/ 2]),
+              )
+          ],
         ),
       );
     } else {
       list.add(
-        SizedBox(
+        Container(
           height: 1,
           width: lineSize,
-          child: Container(
-            color: i < (2 * checkedPoints) - 1
-                ? completedIconColor
-                : incompleteIconColor,
-          ),
+          margin: titles != null ? EdgeInsets.only(bottom: 15) : null,
+          color: i < index ? completedIconColor : incompleteIconColor,
         ),
       );
     }
   }
   return list;
+}
+
+int countCompletedMilestones(List<int> milestones, int value) {
+  int count = 0;
+  for (int milestone in milestones) {
+    if (milestone <= value) {
+      count++;
+    }
+  }
+  return count;
 }
